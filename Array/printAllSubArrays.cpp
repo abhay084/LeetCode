7adb776da1863ccc42571e74c0505f0cb37283f5
Abhay <abhayganvir03@gmail.com>
2023-06-19 19:53:48 +0530

    print all subarrays

---
+++ Array/printAllSubArrays.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void printSubArrays(int arr[],int n){
+
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            for(int k=i;k<=j;k++){
+                cout<<arr[k]<<", ";
+            }
+            cout<<endl;
+        }
+    }
+}
+
+int main(){
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printSubArrays(arr,n);
+}
