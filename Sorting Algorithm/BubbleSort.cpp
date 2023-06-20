6a8ad891612233475855dc8459aac3754621c98d
Abhay <abhayganvir03@gmail.com>
2023-06-21 03:32:06 +0530

    Bubble Sort

---
+++ Sorting Algorithm/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void BubbleSort(int arr[],int n){
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[10] = {0, 3 , 5, -3, -12, 456, 89, 1,1,10};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    BubbleSort(arr,n);
+
+    for(auto it: arr){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
